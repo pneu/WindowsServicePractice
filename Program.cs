c2219577590bac07d18ae0f4bdf5015464989a51
pneu <pneumaster@gmail.com>
2022-03-03 17:16:18 +0900

    initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace HelloService
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            ServiceBase.Run(new UserService1());
+        }
+    }
+
+    public class UserService1 : ServiceBase
+    {
+        public UserService1() {
+            this.ServiceName = "MyHelloService";
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+        protected override void OnStart(string[] args) {
+            base.OnStart(args);
+        }
+    }
+}
